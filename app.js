578ee3ad0ae6de810bf4c3ad6920ce8716dc3b67
Danishalam045 <Danish_Alam@Danish_Alam_045.(none)>
2024-08-14 00:12:12 +0530

    Learning relation in dataBase

--- app.js
+++ app.js
@@ -1,60 +1,3 @@
-// const mongoose = require('mongoose');
-
-// main().catch(err => console.log(err));
-
-// async function main() {
-//     await mongoose.connect('mongodb://127.0.0.1:27017/fruitDB');
-
-//     const fruitSchema = new mongoose.Schema({
-//         name: {
-//             type: String,
-//             required: [true, "Please check your data entry , no name specifiied!"]
-//         },
-//         rating: {
-//             type: Number,
-//             min: 1,
-//             max: 10
-//         },
-//         review: String
-//     });
-
-//     const personSchema = new mongoose.Schema({
-//         name: String,
-//         age: Number
-//     });
-
-//     const Fruit = mongoose.model('Fruit', fruitSchema);
-//     const Person = mongoose.model("Person", personSchema);
-
-//     const fruit = new Fruit(
-//         {
-//             name: 'Apple',
-//             rating: 10,
-//             review: "Pretty solid as a fruit."
-//         }
-//     );
-
-//     // fruit.save();
-
-//     const person = new Person({
-//         name: "John",
-//         age: 37
-//     });
-
-//     // Fruit.updateOne({_id:"654d4eb1e3a5f712815e943c"},{name:"Peach"})
-
-//     console.log(fruit.name);
-//     console.log(person.name);
-
-//     await fruit.save();
-//     // await person.save();
-//     // await Fruit.insertMany([Kiwi,Orange,Banana]);
-
-//     const data = await Fruit.find({});
-//     console.log(data);
-
-// }
-
 const mongoose = require("mongoose");
 
 main().catch((err) => console.log(err));
@@ -62,14 +5,21 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/fruitDB");
   const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+      type: String,
+      required: [true, "Please check your data entry , no name specifiied!"],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 10,
+    },
     review: String,
   });
 
   const Fruit = mongoose.model("Fruit", fruitSchema);
   const fruit = new Fruit({
-    name: "Apple",
+    name: "Papaya",
     rating: 7,
     review: "Pretty Solid as Fruit",
   });
@@ -90,17 +40,35 @@ async function main() {
     review: "Weird Texture!",
   });
   //   await fruit.save();
-  await Fruit.insertMany([kiwi, orange, Banana]);
+  //   await Fruit.insertMany([kiwi, orange, Banana]);
+  //   const data = await Fruit.find({});
+  //   mongoose.connection.close();
+  //   data.forEach((element) => {
+  //     console.log(element.name);
+  //   });
+  //  console.log(data);
+
+  //////////////////////////////      People Schema        ///////////////////////////////
 
   const peopleSchema = new mongoose.Schema({
     name: String,
     age: Number,
+    favouriteFruit: fruitSchema,
+  });
+
+  const pineapple = new Fruit({
+    name: "Pineapple",
+    rating: 9,
+    review: "Great fruit.",
   });
 
   const Person = mongoose.model("Person", peopleSchema);
   const person = new Person({
-    name: "John",
-    age: 42,
+    name: "Amy",
+    age: 12,
+    favouriteFruit: pineapple,
   });
-  //   await person.save();
+//   await person.save();
+  const personData = await Person.find({});
+  console.log(personData);
 }
